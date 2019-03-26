e07833a68351d571dff712cbb9bb777af96bcd3a
ChaoTzuJung <s110319022@stu.ntue.edu.tw>
2019-03-26 17:16:44 +0800

    Simplify List component

--- src/components/molecules/List/__tests__/List.stories.js
+++ src/components/molecules/List/__tests__/List.stories.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { withKnobs, object, boolean } from '@storybook/addon-knobs/react';
+import { withKnobs, text, boolean } from '@storybook/addon-knobs/react';
 import { storiesOf } from '@storybook/react';
 
 import List from 'components/molecules/List';
@@ -11,7 +11,7 @@ stories.addDecorator(withKnobs);
 stories.add('__interactive', () => (
 	<div style={{ margin: '16px' }}>
 		<List
-			text={object('text', 'type something here...')}
+			text={text('text', 'type something here...')}
 			star={boolean('star', false)}
 			edit={boolean('edit', false)}
 			checked={boolean('checked', false)}
--- src/components/molecules/List/index.js
+++ src/components/molecules/List/index.js
@@ -3,26 +3,19 @@ import { contain } from 'react-container-helper';
 import List from './component';
 
 const initState = () => ({
-	todo: {
-		id: Date.now(),
-		content: '下午上課',
-		dateline: '2018/5/14_14:30',
-		file: null,
-		comment: 'meet him lorence Cafe',
-	},
+	text: '',
 });
 
 const mapSetStateToProps = (
-	{ todo },
-	{ className, text, checked, star, edit, date, file, comment, ...other },
+	{ text },
+	{ className, checked, star, edit, date, file, comment, ...other },
 	setState,
 ) => ({
 	// state
-	todo,
+	text,
 
 	// props
 	className,
-	text,
 	checked,
 	star,
 	edit,
