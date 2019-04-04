0a880545bdf6f5931516b4b9eb34b7254cb4a8e7
25sprout-alan <alan@25sprout.com>
2019-04-04 16:29:30 +0800

    feat: Add helper ro transfer date and time

--- src/util/helper.js
+++ src/util/helper.js
@@ -1,1 +1,21 @@
 export const isExist = value => value !== null && value !== '' && typeof value !== 'undefined';
+
+export const transDateToDay = date => {
+	const year = date.getFullYear();
+	const month = date.getMonth() + 1;
+	const day = date.getDate();
+	const hour = date.getHours();
+	const minute = date.getMinutes();
+
+	return {
+		day: `${year}/${month}/${day}`,
+		time: `${hour}:${minute}0`,
+	};
+};
+
+export const transDayToDate = (day, time) => {
+	const now = new Date(`${day} ${time}`);
+	return {
+		date: now,
+	};
+};
